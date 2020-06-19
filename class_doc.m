343e500bb67685caa5f8736ce3cbe4cf0f6fb33c
Abenezer S. Alemu <asa98@pitt.edu>
2020-06-19 13:35:37 -0400

    variable z has been added

--- class_doc.m
+++ class_doc.m
@@ -2,11 +2,14 @@ clc; clear; close all;
 x = 1:0.1:10;
 y = sin(2*x);
 z = cos(2*x);
+w = 2*z;
 
 figure
 plot(x, y)
 hold on
 plot(x, z)
+hold on 
+plot(x, w) 
 grid on
 xlabel('Time(sec)')
 ylabel('Amplitude (Volts)')
