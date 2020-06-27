731af80d8f0f63bed22ef483b187c3b03fea0d61
Abenezer S. Alemu <asa98@pitt.edu>
2020-06-27 14:15:04 -0400

    new z variable has been added.

--- WavePlot.m
+++ WavePlot.m
@@ -1,9 +1,13 @@
 clc; clear; close all;
 x = 1:0.1:10;
 y = 4*sin(2*x);
+z = cos(y);
 
 figure
+hold on 
 plot(x, y)
+plot(x, z)
+hold off 
 grid on
 xlabel('Time(sec)')
 ylabel('Amplitude (Volts)')
